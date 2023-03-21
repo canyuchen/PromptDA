e4ae349b020cbb3d612dc96e149db8462c2f2637
harvardchen <harvardchen97@gmail.com>
2023-03-21 15:07:55 -0500

    update

--- test_promptda_template_manual/test_mr/test_run_roberta_large_template_seed_100_87_42_21_13.sh
+++ test_promptda_template_manual/test_mr/test_run_roberta_large_template_seed_100_87_42_21_13.sh
@@ -5,15 +5,15 @@
 
 
 
-sh run_roberta_large_template_free_seed_100.sh > run_roberta_large_template_free_seed_100.log
+sh run_roberta_large_template_seed_100.sh > run_roberta_large_template_seed_100.log
 
-sh run_roberta_large_template_free_seed_87.sh > run_roberta_large_template_free_seed_87.log
+sh run_roberta_large_template_seed_87.sh > run_roberta_large_template_seed_87.log
 
-sh run_roberta_large_template_free_seed_42.sh > run_roberta_large_template_free_seed_42.log
+sh run_roberta_large_template_seed_42.sh > run_roberta_large_template_seed_42.log
 
-sh run_roberta_large_template_free_seed_21.sh > run_roberta_large_template_free_seed_21.log
+sh run_roberta_large_template_seed_21.sh > run_roberta_large_template_seed_21.log
 
-sh run_roberta_large_template_free_seed_13.sh > run_roberta_large_template_free_seed_13.log
+sh run_roberta_large_template_seed_13.sh > run_roberta_large_template_seed_13.log
 
 
 
