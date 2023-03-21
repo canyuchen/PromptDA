a03b52de3239974c13249ee064650350bf3c2b2d
harvardchen <harvardchen97@gmail.com>
2023-03-21 15:07:19 -0500

    update

--- test_promptda_template/test.sh
+++ test_promptda_template/test.sh
@@ -1,41 +1,41 @@
 
 cd ./test_subj
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_sst_2
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_cr
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_mr
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_cola
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_mpqa
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
 cd ..
 
 cd ./test_sst_5
 
-sh test_run_roberta_large_template_free_seed_100_87_42_21_13.sh
+sh test_run_roberta_large_template_seed_100_87_42_21_13.sh
 
